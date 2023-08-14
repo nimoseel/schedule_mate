e73c7cbfceb8a71ec5e8085719d8878b56fd6765
“nimoseel” <fromsomini@gmail.com>
2023-08-14 11:04:21 +0900

    Feature : schedule 테이블

--- lib/model/schedule.dart
+++ lib/model/schedule.dart
@@ -1,12 +1,16 @@
 import 'package:drift/drift.dart';
 
 class Schedules extends Table {
+  // id
   IntColumn get id => integer().autoIncrement()();
 
+  // 스케쥴 내용
   TextColumn get content => text()();
 
+  // 스케쥴 날짜
   DateTimeColumn get date => dateTime()();
 
+  // 작성 날짜
   DateTimeColumn get createdAt => dateTime().clientDefault(
         () => DateTime.now(),
       )();

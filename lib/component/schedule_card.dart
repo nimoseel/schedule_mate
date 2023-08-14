db30a76e622ab48b0f2cfd961932ad23e9f5f5b5
“nimoseel” <fromsomini@gmail.com>
2023-08-14 15:35:12 +0900

    Fix : statefulWidget, TextFormField로 변경

--- lib/component/schedule_card.dart
+++ lib/component/schedule_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:schedule_mate/const/color.dart';
 
-class ScheduleCard extends StatelessWidget {
+class ScheduleCard extends StatefulWidget {
   final bool isChecked;
   final Function(bool?)? onChanged;
 
@@ -12,32 +12,57 @@ class ScheduleCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ScheduleCard> createState() => _ScheduleCardState();
+}
+
+class _ScheduleCardState extends State<ScheduleCard> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(5.0, 0.0, 25.0, 0.0),
       child: Row(
         children: [
           Checkbox(
-            value: isChecked,
-            onChanged: onChanged,
+            value: widget.isChecked,
+            onChanged: widget.onChanged,
             activeColor: PRIMARY_COLOR,
             side: BorderSide(
               color: PRIMARY_COLOR,
             ),
           ),
-          Expanded(
-            child: TextField(
-              cursorColor: PRIMARY_COLOR,
-              cursorWidth: 1.0,
-              cursorHeight: 20.0,
-              decoration: InputDecoration(
-                iconColor: Colors.pink,
-                border: InputBorder.none,
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.pink,
+          Form(
+            key: formKey,
+            child: Expanded(
+              child: TextFormField(
+                validator: (String? val){
+                  if(val == null || val.isEmpty){
+                    return '값을 입력해주세요';
+                  }
+                  return null;
+                },
+                cursorColor: PRIMARY_COLOR,
+                cursorWidth: 1.0,
+                cursorHeight: 20.0,
+                decoration: InputDecoration(
+                  iconColor: Colors.pink,
+                  border: InputBorder.none,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.pink,
+                    ),
                   ),
                 ),
+                onFieldSubmitted: (String value) {
+                  if(formKey.currentState == null){
+                    return;
+                  }
+                  if(formKey.currentState!.validate()){
+                    print('에러없음');
+                  }else{
+                    print('에러있음');
+                  }
+                },
               ),
             ),
           ),

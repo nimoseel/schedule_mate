89fa8229abcd5564fae711c454691dd060d7850d
“nimoseel” <fromsomini@gmail.com>
2023-08-16 23:11:46 +0900

    Feature : GetIt 으로 의존성 주입

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:drift/drift.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:schedule_mate/database/drift_database.dart';
 import 'package:schedule_mate/screen/home_screen.dart';
@@ -9,6 +11,7 @@ void main() async{
   await initializeDateFormatting();
 
   final database = LocalDatabase();
+  GetIt.I.registerSingleton<LocalDatabase>(database);
 
   runApp(
     MaterialApp(

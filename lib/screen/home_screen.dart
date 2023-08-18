805370dee5d922085292eab6aa95c515b01173cf
“nimoseel” <fromsomini@gmail.com>
2023-08-18 17:05:42 +0900

    Fix : isCreate 상태 추가, _ScheduleList의 Padding값 삭제

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -21,10 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
   );
   DateTime focusedDay = DateTime.now();
 
+  bool isCreate = false;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
@@ -45,9 +47,15 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: 15.0,
               ),
-              // ReorderableListView.builder
+              isCreate
+                  ? ScheduleCard(
+                    selectedDate: selectedDay,
+                    isChecked: false,
+                  )
+                  : Container(),
               _ScheduleList(
                 selectedDate: selectedDay,
+                isCreate: isCreate,
               ),
             ],
           ),
@@ -56,7 +64,11 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: PRIMARY_COLOR,
           splashColor: PRIMARY_COLOR[600],
           child: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              isCreate = true;
+            });
+          },
         ),
       ),
     );
@@ -72,46 +84,46 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class _ScheduleList extends StatelessWidget {
   final DateTime selectedDate;
+  final bool isCreate;
 
   const _ScheduleList({
     required this.selectedDate,
+    required this.isCreate,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0),
-        child: StreamBuilder<List<Schedule>>(
-            stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
+      child: StreamBuilder<List<Schedule>>(
+          stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-              if (snapshot.hasData && snapshot.data!.isEmpty) {
-                return Center(
-                  child: Text('스케줄이 없습니다.'),
-                );
-              }
-
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  // print(snapshot.data);
-                  final schedule = snapshot.data![index];
-
-                  return ScheduleCard(
-                    selectedDate: schedule.date,
-                    isChecked: schedule.done,
-                    content: schedule.content,
-                    scheduleId: schedule.id,
-                  );
-                },
+            if (snapshot.hasData && snapshot.data!.isEmpty && !isCreate) {
+              return Center(
+                child: Text('스케줄이 없습니다.'),
               );
-            }),
-      ),
+            }
+
+            return ListView.builder(
+              // ReorderableListView.builder
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                // print(snapshot.data);
+                final schedule = snapshot.data![index];
+
+                return ScheduleCard(
+                  selectedDate: schedule.date,
+                  isChecked: schedule.done,
+                  content: schedule.content,
+                  scheduleId: schedule.id,
+                );
+              },
+            );
+          }),
     );
   }
 }
